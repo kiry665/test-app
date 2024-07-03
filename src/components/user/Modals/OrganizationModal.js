0164441bf78f50ef1eab4ad4c9876ee44b66b2a2
novok <novokshonv.kirill@yandex.ru>
2024-07-03 19:40:08 +0500

    change modal

--- src/components/user/Modals/OrganizationModal.js
+++ src/components/user/Modals/OrganizationModal.js
@@ -1,22 +1,31 @@
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
+import styles from '../../../styles/OrganizationModal.module.css'
+import { useState } from "react";
+import { Table, Button, Modal } from 'react-bootstrap';
 
 function OrganizationModal({show, onHide}){
+
+    const leftColumnData = ['Организация', 'Договор №', 'Дата договора', 'Дата окончания договора', 'Врямя доступа', 'ФИО', 'Дожность'];
+
+    const [rightColumnData, setRightColumnData] = useState(['Билдинг', '123', '03.07.2024', '03.07.2025', '8:00-21:00', 'Иван Иванович Иванов', '213asd']);
+
     return(
         <Modal show={show} onHide={onHide}>
         <Modal.Dialog>
-            <Modal.Header closeButton>
-            <Modal.Title>Modal title</Modal.Title>
-            </Modal.Header>
-
             <Modal.Body>
-            <p>Modal body text goes here.</p>
+                <h1>Основание для допуска</h1>
+                <br></br>
+                <Table bordered>
+                    <tbody>
+                        {leftColumnData.map((leftData, index) => (
+                            <tr key={index}>
+                                <td className={styles.leftColumn}>{leftData}</td>
+                                <td>{rightColumnData[index]}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </Table>
+                <Button className={styles.button} variant="primary" onClick={onHide}>Закрыть</Button>
             </Modal.Body>
-
-            <Modal.Footer>
-            <Button variant="secondary">Close</Button>
-            <Button variant="primary">Save changes</Button>
-            </Modal.Footer>
         </Modal.Dialog>
         </Modal>
     );
